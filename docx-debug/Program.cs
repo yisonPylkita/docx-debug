a5860c9f802a3672c2fd38258c55c65a05bcc119
Wojciech Bartnik <yisonPylkita@gmail.com>
2018-01-16 20:14:17 +0100

    Project init

---
+++ docx-debug/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Validation;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+
+namespace docx_debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\arlen\Downloads\broken-file.docx";
+            ValidateWordDocument(filepath);
+            Console.WriteLine("The file is valid so far.");
+            Console.WriteLine("Inserting some text into the body that would cause Schema error");
+            Console.ReadKey();
+
+            ValidateCorruptedWordDocument(filepath);
+            Console.WriteLine("All done! Press a key.");
+            Console.ReadKey();
+        }
+
+        public static void ValidateWordDocument(string filepath)
+        {
+            using (WordprocessingDocument wordprocessingDocument =
+            WordprocessingDocument.Open(filepath, true))
+            {
+                try
+                {
+                    OpenXmlValidator validator = new OpenXmlValidator();
+                    int count = 0;
+                    foreach (ValidationErrorInfo error in
+                        validator.Validate(wordprocessingDocument))
+                    {
+                        count++;
+                        Console.WriteLine("Error " + count);
+                        Console.WriteLine("Description: " + error.Description);
+                        Console.WriteLine("ErrorType: " + error.ErrorType);
+                        Console.WriteLine("Node: " + error.Node);
+                        Console.WriteLine("Path: " + error.Path.XPath);
+                        Console.WriteLine("Part: " + error.Part.Uri);
+                        Console.WriteLine("-------------------------------------------");
+                    }
+
+                    Console.WriteLine("count={0}", count);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                wordprocessingDocument.Close();
+            }
+        }
+
+        public static void ValidateCorruptedWordDocument(string filepath)
+        {
+            // Insert some text into the body, this would cause Schema Error
+            using (WordprocessingDocument wordprocessingDocument =
+            WordprocessingDocument.Open(filepath, true))
+            {
+                // Insert some text into the body, this would cause Schema Error
+                Body body = wordprocessingDocument.MainDocumentPart.Document.Body;
+                Run run = new Run(new Text("some text"));
+                body.Append(run);
+
+                try
+                {
+                    OpenXmlValidator validator = new OpenXmlValidator();
+                    int count = 0;
+                    foreach (ValidationErrorInfo error in
+                        validator.Validate(wordprocessingDocument))
+                    {
+                        count++;
+                        Console.WriteLine("Error " + count);
+                        Console.WriteLine("Description: " + error.Description);
+                        Console.WriteLine("ErrorType: " + error.ErrorType);
+                        Console.WriteLine("Node: " + error.Node);
+                        Console.WriteLine("Path: " + error.Path.XPath);
+                        Console.WriteLine("Part: " + error.Part.Uri);
+                        Console.WriteLine("-------------------------------------------");
+                    }
+
+                    Console.WriteLine("count={0}", count);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
